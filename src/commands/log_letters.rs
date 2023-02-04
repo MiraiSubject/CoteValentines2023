f9fbffba463665f2df966d3235b855ba01acf5fa
Shivam <s.r.v.ramkisoen@umail.leidenuniv.nl>
2023-02-04 13:12:48 +0100

    Remove logging from public facing channel because of publish

--- src/commands/log_letters.rs
+++ src/commands/log_letters.rs
@@ -3,26 +3,7 @@ use serenity::{model::prelude::ChannelId, prelude::Context};
 use super::send::ValentineLetter;
 
 pub async fn log_letter(ctx: &Context, letter: &ValentineLetter) {
-    let public_facing_channel_id = ChannelId(336524751860138005);
     let audit_channel_id = ChannelId(610201663382487065);
-
-    let public_message = ChannelId::send_message(public_facing_channel_id, &ctx.http, |m| {
-        m.embed(|e| {
-            e.title(if letter.anon {
-                format!("To {}", letter.recipient)
-            } else {
-                format!("From {} to {}", letter.sender, letter.recipient)
-            })
-
-            .description(&letter.letter)
-                .footer(|f| f.text("2023 Classroom of the Elite Valentine's Event"))
-        })
-    })
-    .await;
-
-    if let Err(why) = public_message {
-        println!("Error sending message: {:?}", why);
-    }
 
     let audit_message = ChannelId::send_message(audit_channel_id, &ctx.http, |m| {
         m.embed(|e| {
