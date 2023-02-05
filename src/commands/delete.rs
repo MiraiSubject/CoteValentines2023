1fec6fb3dab4a24b5aed4201122d99ac820c9fb8
oralekin <oralekin@gmail.com>
2023-02-05 17:22:53 +0300

    edit deleted letter embeds to show they are deleted
    handle delete button interaction

--- src/commands/delete.rs
+++ src/commands/delete.rs
@@ -4,27 +4,47 @@ use serenity::{
     prelude::Context,
 };
 
+use crate::{model::Letter, schema::letters::all_columns};
+
 pub async fn run(
     mut interaction: MessageComponentInteraction,
     ctx: &Context,
     db_conn: &mut SqliteConnection,
 ) {
-    delete_letter(interaction.message.id, db_conn).unwrap();
-    interaction.message.edit(ctx, |edit| {
-        edit.content(
-            format!("deleted by {} at {}", interaction.user.name, chrono::prelude::Utc::now().to_rfc3339())
-        ).components(|components| components)
-    }).await.unwrap();
+    let deleted = delete_letter(interaction.message.id, db_conn).expect("Can delete letter");
+    interaction
+        .message
+        .edit(ctx, |edit| {
+            use ellipse::Ellipse;
+            edit.content(format!(
+                "deleted by {} at {}",
+                interaction.user.name,
+                chrono::prelude::Utc::now().to_rfc3339()
+            ))
+            .components(|components| components)
+            .embed(|e| {
+                e.title(if deleted.anon {
+                    format!(
+                        "DELETED: ANONYMOUSLY SENT: From {} to {}",
+                        deleted.sender, deleted.recipient
+                    )
+                } else {
+                    format!("DELETED: From {} to {}", deleted.sender, deleted.recipient)
+                })
+                .description(&deleted.content.as_str().truncate_ellipse(50))
+                .footer(|f| f.text("2023 Classroom of the Elite Valentine's Event"))
+                .color((255, 0, 0))
+            })
+        })
+        .await
+        .unwrap();
 }
 
-fn delete_letter(to_delete: MessageId, conn: &mut SqliteConnection) -> Result<bool, String> {
+fn delete_letter(to_delete: MessageId, conn: &mut SqliteConnection) -> Result<Letter, String> {
     use crate::schema::letters::dsl::*;
 
     diesel::delete(letters.filter(message_id.eq(to_delete.to_string())))
-        .execute(conn)
-        .map_err(|e| {
-            format!("Error {e}")
-        })
-        .map(|deleted_count| deleted_count != 0)
-        
+        .returning(all_columns)
+        .get_result(conn)
+        .map_err(|e| format!("Error {e}"))
 }
